bbb0cbad8820c739c859b4b4a34ded9f6092d7b7
Michael Watson <mdwatson988@gmail.com>
2022-04-28 19:25:33 -0400

    logout button, fix cookies

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -1,46 +1,38 @@
 import React, { useState, useEffect, useContext } from 'react';
 import UserContext from './UserDetails';
 import { Link, Navigate, useNavigate } from 'react-router-dom';
-
 const Navbar = (props) => {
   const user = useContext(UserContext).user;
   const setUser = useContext(UserContext).setUser;
   let navigate = useNavigate();
-
   const handleLogout = () => {
     ``;
-    user = setUser(undefined);
-    navigate('/');
+    setUser(undefined);
+    navigate('/login');
   };
   const handleDelete = () => {
-
+    ``;
     navigate('/deleteUser');
   };
   const handleFavorites = () => {
     ``;
     navigate('/favorites');
   };
-
+  const handleSearch = () => {
+    ``;
+    navigate('/search');
+  };
   return (
     <>
       <header>
         <div className='brand nav'>FindMyBrews&#127867;</div>
-        {/*Leaving this as a UL/LI so that we want to add more navbar options */}
-        <ul className='nav_links'>
-          {user && (
-            <li>
-              <Link to='/logout' className='logout-Btn' onClick={handleLogout}>
-                Logout
-              <option className='delete-Btn' onClick={handleDelete}>
-                Delete Account
-              </option>
-            </Link>
-            </li>
-          )}
-        </ul>
+        {user && (
+          <button className='logout-Btn' onClick={handleLogout}>
+            Logout
+          </button>
+        )}
       </header>
     </>
   );
 };
-
 export default Navbar;
